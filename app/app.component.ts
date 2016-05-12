433478d4cabf1ddff042d4fc6092081b6264ad48
sally pang <sallyxpang_@live.com>
2016-05-12 13:51:32 -0700

    Added files

---
+++ app/app.component.ts
@@ -0,0 +1,105 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+	id: number;
+	name: string;
+}
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>{{title}}</h1>
+
+  <h2>Storm's Heroes</h2>
+  <ul class="heroes">
+  	<li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" 
+  	(click)="onSelect(hero)">
+  		<span class="badge">{{hero.id}}</span>{{hero.name}}
+  	</li>
+  </ul>
+
+  <div *ngIf="selectedHero">
+  	<h2>{{selectedHero.name}} details</h2>
+  	<div>
+  		<label>id: </label>{{selectedHero.id}}
+  	</div>
+  	<div>
+  		<label>name: </label>
+  		<input [(ngModel)]="selectedHero.name" placeholder="name">
+  	</div>
+  </div>
+  `,
+  styles:[`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
+
+})
+export class AppComponent {
+	title = 'Tour of Heroes';
+	selectedHero: Hero;
+	heroes = HEROES;
+
+	onSelect(hero: Hero) {
+		this.selectedHero = hero;
+	}
+}
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Cyberman" },
+  { "id": 12, "name": "Stormburst" },
+  { "id": 13, "name": "Magna" },
+  { "id": 14, "name": "Claire/Sonar" },
+  { "id": 15, "name": "Battle" },
+  { "id": 16, "name": "Akmal" },
+  { "id": 17, "name": "Emerald" },
+  { "id": 18, "name": "Jin" },
+  { "id": 19, "name": "Ambrun" },
+  { "id": 20, "name": "Stephanie" }
+];
+
